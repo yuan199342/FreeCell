11720bdc474c3fb82a60707c22d0d4f1347cefca
Yuan <yuan199342@gmail.com>
2019-11-23 01:27:34 +0800

    下方牌區堆疊---完成 下方牌區 一次多張移動---完成 左上牌區移動---完成 右上牌區移動---完成
    未完成
    計時功能
    暫停功能
    返回功能
    遊戲結束功能
    移動牌時的特效

--- js.js
+++ js.js
@@ -67,8 +67,11 @@ switch(cardindex % 13){
 		break
 }
 	}
+
+
 //將BottomDeck物件創造出來
 const BottomArea = document.querySelector(".BottomArea")
+const TopArea = document.querySelector(".TopArea")
 BottomArea.style.display = 'flex'
 
 function createCard() {
@@ -85,17 +88,20 @@ function createCard() {
 		decks.forEach((card,cardindex) => {
 			const cardElem = document.createElement('div')
 			cardElem.style.position = 'absolute'
-			cardElem.style.width = '90%'
+			cardElem.style.width = '130px'
 			cardElem.style.height = '200px'
-			cardElem.style.border ='1px solid #c9c9c9'
+			cardElem.style.border ='1px solid white'
 			cardElem.style.color = transformNumberToBackgroundColor(card)
 			cardElem.style.backgroundColor = 'black'
 			cardElem.style.padding = '5px'
 			cardElem.style.borderRadius = '5px'
-			cardElem.style.top = cardindex * 30 + 'px'
+			cardElem.style.top =cardindex * 30 +'px'
 			cardElem.cardNumber = card
 			cardElem.deckIndex = index
 			cardElem.innerHTML = changeNumberToPoker(card) + transformNumberToColor(card)
+			if(cardindex === decks.length -1){
+				cardElem.draggable = true
+			}
 			bottomDeckElem.appendChild(cardElem) 
 		})
 		BottomArea.appendChild(bottomDeckElem)
@@ -103,6 +109,109 @@ function createCard() {
 	
 }
 createCard()
+
+//創造上方兩個牌區
+const LeftTopArea = document.querySelector('.LeftTopArea')
+LeftTopArea.style.display = 'flex'
+for(i=0;i<4;i++){
+	const LeftTopElem = document.createElement('div')
+			LeftTopElem.style.width = '100%'
+			LeftTopElem.style.height = '200px'
+			LeftTopElem.style.border ='1px solid black'
+			LeftTopArea.appendChild(LeftTopElem)
+}
+
+const RightTopArea = document.querySelector('.RightTopArea')
+RightTopArea.style.display = 'flex'
+for(i=0;i<4;i++){
+	const RightTopElem = document.createElement('div')
+			RightTopElem.style.width = '100%'
+			RightTopElem.style.height = '200px'
+			RightTopElem.style.border ='1px solid black'
+			RightTopArea.appendChild(RightTopElem)
+}
+
+//如果是最後一張牌 就加上draggable
+function checkLastCard(){
+	var deck  = BottomArea.firstChild
+	for(i=0;i<BottomArea.childElementCount;i++){
+		deck.lastElementChild.draggable = true
+		console.log(deck.lastElementChild.draggable)
+		deck = deck.nextElementSibling
+	}
+}
+
+//拖拉功能
+let data 
+function dragCard(e){
+	e.preventDefault()
+	data = e.target
+}
+function dropCard(e){
+	e.preventDefault()
+	
+	//下方牌區堆疊
+	if(((data.cardNumber+1) % 13  == e.target.cardNumber % 13) && (data.style.color != e.target.style.color) && (e.target.parentElement.parentElement == BottomArea) && (e.target.nextElementSibling == null)){
+		do{
+		var data2 = data.nextElementSibling
+		e.target.parentNode.appendChild(data)
+		data.style.top = (parseInt(data.previousElementSibling.style.top)+30) + "px"
+		data = data2
+		console.log(data)
+		}while(data != null)
+		console.log('下方牌區堆疊')
+	}
+	//放到左上牌區
+	//bug 放到牌與牌區旁的縫隙 依然可以堆疊
+	if((e.target.parentElement == LeftTopArea) && !data.nextElementSibling){
+		console.log(e.target)
+		data.style.top = '0px'
+		e.target.appendChild(data)
+		console.log('放到左上牌區')
+	}
+	//四種 A 放到右上牌區
+	if(e.target.parentElement == RightTopArea){
+		switch(data.cardNumber){
+			case 1:
+				RightTopArea.firstElementChild.appendChild(data)
+				data.style.top = '0px'
+				break
+			case 14:
+				RightTopArea.firstElementChild.nextElementSibling.appendChild(data)
+				data.style.top = '0px'
+				break
+			case 27:
+				RightTopArea.lastElementChild.previousElementSibling.appendChild(data)
+				data.style.top = '0px'
+				break
+			case 40:
+				RightTopArea.lastElementChild.appendChild(data)
+				data.style.top = '0px'
+				break
+		}
+		console.log('四種 A 放到右上牌區')
+	}
+	//放完 A 後 其他牌放上右上牌區
+	if((e.target.cardNumber == (data.cardNumber -1)) && e.target.parentElement.parentElement == RightTopArea){
+		e.target.parentElement.appendChild(data)
+		data.style.top = '0px'
+		console.log('放完 A 後 其他牌放上右上牌區')
+	}
+	
+	
+	checkLastCard()
+}
+function allowDrop(e){
+	e.preventDefault()
+}
+
+
+//事件監聽拖拉功能
+const container = document.querySelector('.container')
+container.addEventListener('drag' ,dragCard)
+container.addEventListener('drop' ,dropCard)
+container.addEventListener('dragover' ,allowDrop)
+
 
 
 
